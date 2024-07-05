0db9d205b89fca67c815ea7c6d6e0005c02a53f2
Nour <noursalm261@gmail.com>
2024-07-05 03:22:56 +0300

    buttons using grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,25 @@
 آلة حاسبه
   </title>
  </svelte:head>
- <div class="bg-white h-[30rem] w-[20rem] rounded-2xl">
-  <div class="bg-black h-[5rem] w-[5rem] rounded-full flex justify-center">
-
-  
-  </div>
+ <div class="bg-white h-fit w-fit rounded-2xl grid grid-cols-4 gap-7">
+  <button>%</button>
+  <button>√</button>
+  <button>CE</button>
+  <button>C</button>
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  <button>-</button>
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button>÷</button>
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>x</button>
+  <button>.</button>
+  <button>0</button>
+  <button>=</button>
+  <button>+</button>
  </div>
